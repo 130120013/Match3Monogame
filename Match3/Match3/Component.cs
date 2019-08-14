0667f3d69eb33156ca274f5477ca2ddd41bb3b80
Алина Протопопова <alinap95@yandex.ru>
2019-08-14 18:05:18 +1000

    initial commit - drew button

---
+++ Match3/Match3/Component.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace Match3
+{
+  public abstract class Component: IDisposable
+  {
+    public abstract void Draw(GameTime gameTime, SpriteBatch spriteBatch);
+
+    public abstract void Update(GameTime gameTime);
+
+        public abstract void Dispose();
+  }
+}
---
+++ Match3/Match3/Controls/Button.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Match3
+{
+      public class Button : Component
+      {
+ #region Fields
+
+        private MouseState currentMouse;
+
+        private SpriteFont font;
+
+        private bool isHovering;
+
+        private MouseState previousMouse;
+
+        private Texture2D texture;
+
+        #endregion
+
+        #region Properties
+
+        private bool disposed = false;
+
+        public event EventHandler Click;
+
+        public bool Clicked { get; private set; }
+
+        public Color PenColour { get; set; }
+
+        public Vector2 Position { get; set; }
+
+        public Rectangle Rectangle { get; set; }
+
+        public string Text { get; set; }
+
+#endregion
+
+#region Methods
+
+        public Button(Texture2D tx, SpriteFont fnt)
+        {
+          texture = tx;
+
+          font = fnt;
+
+          PenColour = Color.Black;
+
+          Rectangle = new Rectangle((int)Position.X, (int)Position.Y, texture.Width, texture.Height);
+        }
+
+        public Button()
+        {
+
+        }
+
+        public override void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        // Protected implementation of Dispose pattern.
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+
+            disposed = true;
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            if (disposed == true)
+            {
+                var colour = Color.White;
+
+                if (isHovering)
+                    colour = Color.Gray;
+
+                spriteBatch.Draw(texture, Rectangle, colour);
+
+                if (!string.IsNullOrEmpty(Text))
+                {
+                    var x = (Rectangle.X + (Rectangle.Width / 2)) - (font.MeasureString(Text).X / 2);
+                    var y = (Rectangle.Y + (Rectangle.Height / 2)) - (font.MeasureString(Text).Y / 2);
+
+                    spriteBatch.DrawString(font, Text, new Vector2(x, y), PenColour);
+                }
+            }
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+          previousMouse = currentMouse;
+          currentMouse = Mouse.GetState();
+
+          var mouseRectangle = new Rectangle(currentMouse.X, currentMouse.Y, 1, 1);
+
+          isHovering = false;
+
+          if(mouseRectangle.Intersects(Rectangle))
+          {
+            isHovering = true;
+
+            if(currentMouse.LeftButton == ButtonState.Released && previousMouse.LeftButton == ButtonState.Pressed)
+            {
+              Click?.Invoke(this, new EventArgs());
+            }
+          }
+        }
+
+        #endregion
+      }
+}
